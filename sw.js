b8dadc863db177478f0256a8e80ea84eb1edc5e2
yonerbimp-source <yonerbimp@gmail.com>
2026-07-24 23:08:13 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'sobres-cache-v7';
-const FILES = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
